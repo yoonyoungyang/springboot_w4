053e2124206c5af652e799f577ccbc8d4a3122b9
yang-yun-yeong <youngonly0118@gmail.com>
2026-07-28 21:06:52 +0900

    9주차 리팩토링 요소 적용

--- src/main/java/kr/adapterz/springboot/dto/CommentListResponse.java
+++ src/main/java/kr/adapterz/springboot/dto/CommentListResponse.java
@@ -1,46 +1,4 @@
 package kr.adapterz.springboot.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import kr.adapterz.springboot.entity.Comment;
-import lombok.Getter;
-
-import java.time.LocalDateTime;
-
-@Getter
 public class CommentListResponse {
-
-    @JsonProperty("comment_id")
-    private Long commentId;
-
-    private String content;
-
-    @JsonProperty("created_at")
-    private LocalDateTime createdAt;
-
-    @JsonProperty("updated_at")
-    private LocalDateTime updatedAt;
-
-    @JsonProperty("user_id")
-    private Long userId;
-
-    private String nickname;
-
-    @JsonProperty("profile_img")
-    private String profileImg;
-
-    @JsonProperty("is_mine")
-    private boolean isMine;
-
-    public CommentListResponse(Comment comment, Long loginUserId ) {
-        this.commentId = comment.getCommentId();
-        this.content = comment.getContent();
-        this.createdAt = comment.getCreatedAt();
-        this.updatedAt = comment.getUpdatedAt();
-        this.userId = comment.getUser().getUserId();
-        this.nickname = comment.getUser().getNickname();
-        this.profileImg = comment.getUser().getProfileImg();
-
-        this.isMine = comment.getUser().getUserId().equals(loginUserId);
-
-    }
 }
